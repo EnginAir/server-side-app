03e6c6d0d64f16c461df3c21b90fe8902eb615e5
duper51 <ianotto24@gmail.com>
2020-01-28 19:09:44 -0700

    initial commit

---
+++ src/main/java/executors/ImportExecutor.java
@@ -0,0 +1,38 @@
+/*
+ * The MIT License
+ *
+ * Copyright © 2010-2020 EnginAir Team
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package executors;
+
+import dev.morphia.Datastore;
+import dev.morphia.Morphia;
+
+import java.util.HashMap;
+
+/**
+ * Created by ianot on 1/28/2020. None of this software may be reproduced without
+ * the express written permission of PlaygroundMC.
+ */
+public class ImportExecutor extends Executor {
+    public ImportExecutor(HashMap<String, String> config) {
+        super(config);
+    }
+
+    public boolean execute() {
+        //TODO
+        return false;
+    }
+
+    private Datastore makeConnection() {
+        //TODO
+        return null;
+    }
+}
