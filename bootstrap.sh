63cb496fa5da1cc6f9b0f0f8f69ece05d98c7f6b
Ian Otto <ian@duper51.me>
2020-04-16 22:05:36 -0700

    small change

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,8 @@ if [ -z $MAX_RAM ]; then
 MAX_RAM=1G
 fi
 
-if [ -z $MIN_RAM ]; then
-MIN_RAM=128M
+if [ -z $START_RAM ]; then
+START_RAM=128M
 fi
 
 java -Xmx$MAX_RAM -Xms$START_RAM -jar server-side-app.jar $@
